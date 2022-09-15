a743b6fa30b1d8b18d19a02418e3ada85d82e713
hosyaeuw <buyclock@mail.ru>
2022-09-15 18:57:01 +0300

    feat: made up chat

---
+++ handlebars.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+    data: path.join(__dirname, 'src/data'),
+    layouts: path.join(__dirname, 'src/pages'),
+    partials: path.join(__dirname, 'src/components'),
+    helpers: path.join(__dirname, 'src/helpers'),
+};
