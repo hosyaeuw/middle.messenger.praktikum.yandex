005194217684595b56636b119ac0b5992c1431d2
hosyaeuw <buyclock@mail.ru>
2022-10-28 23:20:22 +0300

    refactor: create error pages

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Auth, Reg, Chat, Settings, ErrorPage } from 'pages';
+import { Auth, Reg, Chat, Settings, ErrorPage, Page404, ServerErrorPage } from 'pages';
 import { TAccess } from 'router/Router';
 import {
     Button,
@@ -29,24 +29,7 @@ registerComponent(Menu);
 registerComponent(Modal);
 registerComponent(AddUsersToChatModal);
 registerComponent(DeleteUsersFromChatModal);
-
-// export const routers = {
-//     [Path.home]: () => window.location.replace(Path.auth),
-//     [Path.auth]: () => new Auth(),
-//     [Path.reg]: () => new Reg(),
-//     [Path.chat]: () => new Chat(),
-//     [Path.settings]: () => new Settings(),
-//     [Path.serverError]: () =>
-//         new ErrorPage({
-//             title: '500',
-//             description: 'We already know about the problem and are trying to solve it',
-//         }),
-//     [Path.another]: () =>
-//         new ErrorPage({
-//             title: '404',
-//             description: 'this page does not exist',
-//         }),
-// };
+registerComponent(ErrorPage);
 
 router
     .setPublicRedirect(Path.messenger)
@@ -58,6 +41,6 @@ router
     .register(Path.messenger, Chat, TAccess.protected)
     .register(Path.message, Chat, TAccess.protected)
     .register(Path.settings, Settings, TAccess.protected)
-    .register(Path.serverError, ErrorPage, TAccess.protected)
-    .register(Path.another, ErrorPage, TAccess.protected)
+    .register(Path.serverError, ServerErrorPage, TAccess.protected)
+    .register(Path.another, Page404, TAccess.protected)
     .compile();
